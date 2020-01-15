c0e7e95352d8b58abcc8c6821eef752de25d00be
mrlinh9988 <mrlinhdeptrai9988@gmail.com>
2020-01-15 16:28:25 +0700

    03

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', num: 2212222 }); 
+  res.render('index', { title: 'Express', num: 333 }); 
 });
 
 module.exports = router;
